d6e0e922374e6f6d70426a8912a408eac67d6698
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2024-11-22 17:10:57 +0100

    added initcmds.py and fixtures for Luogo, Evento

---
+++ initcmds.py
@@ -0,0 +1,52 @@
+import json
+from eventi.models import *
+
+
+def erase_db():
+    print("Cancello il DB")
+    Luogo.objects.all().delete()
+    Evento.objects.all().delete()
+
+
+def init_db():
+    if len(Evento.objects.all()) != 0:
+        return
+
+    # prova a importare fixtures da file
+    try:
+        fixture_luoghi = open('fixtures/luoghi.json', 'r')
+        fixture_eventi = open('fixtures/eventi.json', 'r')
+
+        luoghi = json.load(fixture_luoghi)
+        eventi = json.load(fixture_eventi)
+
+        fixture_luoghi.close()
+        fixture_eventi.close()
+    except OSError as e:
+        print("Errore nell'importazione delle fixture! Interrompo il popolamento...", e)
+        return
+
+    for luogo in luoghi:
+        new_luogo = Luogo()
+        new_luogo.nome = luogo['nome']
+        new_luogo.descrizione = luogo['descrizione']
+        new_luogo.indirizzo = luogo['indirizzo']
+        new_luogo.sito_web = luogo['sito_web']
+        new_luogo.save()
+
+    for evento in eventi:
+        new_evento = Evento()
+        new_evento.titolo = evento['titolo']
+        new_evento.descrizione = evento['descrizione']
+        new_evento.posti = evento['posti']
+        new_evento.luogo = Luogo.objects.get(nome__exact=evento['luogo'])
+        new_evento.save()
+        new_evento = Evento.objects.get(titolo__exact=evento['titolo'])
+        for tag in evento['tag']:
+            new_evento.tags.add(tag)
+        new_evento.save()
+
+    print("DUMP DB")
+    print(Luogo.objects.all())
+    print(Evento.objects.all())
+    print(f'Caricati {Luogo.objects.all().count()} luoghi e {Evento.objects.all().count()} eventi')
