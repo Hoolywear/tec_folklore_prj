8379cad01505838113cc7b601703a2831954453f
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2024-11-29 13:27:18 +0100

    add email sending signals for booking and waitlist deletions

--- eventi/signals.py
+++ eventi/signals.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
-from eventi.models import Prenotazione, Evento
+from eventi.models import Prenotazione, Evento, AttesaEvento
 
 
 @receiver(pre_delete, sender=Prenotazione)
@@ -20,3 +20,23 @@ def send_waitlist_emails(sender, instance, **kwargs):
             )
     except Evento.DoesNotExist:
         print("error")
+
+
+@receiver(pre_delete, sender=Prenotazione)
+def send_delete_prenotazione_email(sender, instance, **kwargs):
+    send_mail(
+        "Hai eliminato una prenotazione!",
+        f"Hai eliminato la tua prenotazione per {instance.evento}",
+        "prenotazioni@hubfolklore.it",
+        [instance.utente.email]
+    )
+
+
+@receiver(pre_delete, sender=AttesaEvento)
+def send_delete_attesa_email(sender, instance, **kwargs):
+    send_mail(
+        "Non sei più in lista d'attesa!",
+        f"Hai cancellato la tua iscrizione alla lista d'attesa per {instance.evento}.",
+        "waitlist@hubfolklore.it",
+        [instance.utente.email]
+    )
