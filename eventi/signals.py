10788d1eceb384353b931f5105d07241593cf3db
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2024-11-29 13:30:58 +0100

    add email sending signals for event bookings and waitlist subscriptions

--- eventi/signals.py
+++ eventi/signals.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
+from django.template.defaultfilters import pluralize
 
 from eventi.models import Prenotazione, Evento, AttesaEvento
 
@@ -27,7 +28,7 @@ def send_delete_prenotazione_email(sender, instance, **kwargs):
     send_mail(
         "Hai eliminato una prenotazione!",
         f"Hai eliminato la tua prenotazione per {instance.evento}",
-        "prenotazioni@hubfolklore.it",
+        "eventi@hubfolklore.it",
         [instance.utente.email]
     )
 
@@ -37,6 +38,26 @@ def send_delete_attesa_email(sender, instance, **kwargs):
     send_mail(
         "Non sei più in lista d'attesa!",
         f"Hai cancellato la tua iscrizione alla lista d'attesa per {instance.evento}.",
+        "waitlist@hubfolklore.it",
+        [instance.utente.email]
+    )
+
+
+@receiver(post_save, sender=Prenotazione)
+def send_event_booking_email(sender, instance, **kwargs):
+    send_mail(
+        "Hai prenotato un evento!",
+        f"Hai prenotato {instance.posti} {pluralize(instance.posti, 'posto,posti')} per {instance.evento}.",
+        "eventi@hubfolklore.it",
+        [instance.utente.email]
+    )
+
+
+@receiver(post_save, sender=AttesaEvento)
+def send_waitlist_subscription_email(sender, instance, **kwargs):
+    send_mail(
+        "Ti sei iscritto alla lista d'attesa!",
+        f"Ti sei iscritto alla lista d'attesa per {instance.evento}.",
         "waitlist@hubfolklore.it",
         [instance.utente.email]
     )
