3320b835cf8d9ba1d0275c38b7425fb87712eab7
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2024-12-03 10:36:23 +0100

    comment eventi/imgutils.py

--- eventi/imgutils.py
+++ eventi/imgutils.py
@@ -4,11 +4,13 @@ import os
 
 
 def save_rename_luogo_img(instance, filename):
+    # recupero l'estensione del file
     old_ext = os.path.splitext(filename)[1]
     os.rename(filename, 'luogo_thumb_' + str(instance.pk) + old_ext)
     return os.path.join('thumbnails/luoghi', filename)
 
 
 def save_rename_evento_img(instance, filename):
+    # recupero l'estensione del file
     old_ext = os.path.splitext(filename)[1]
     return os.path.join('thumbnails/eventi', 'evento_thumb_' + str(instance.pk) + old_ext)
