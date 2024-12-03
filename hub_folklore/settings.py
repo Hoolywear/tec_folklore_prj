c02b34bc5a3a92f064cbceca43d57d0a5b0ab5d8
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2024-12-03 10:16:18 +0100

    add static and media settings

--- hub_folklore/settings.py
+++ hub_folklore/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -140,3 +141,6 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     # email degli admin
     ADMINS = [("Iacopo", "iacopo@admin")]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
